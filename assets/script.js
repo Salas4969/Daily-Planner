9d2adddb7b56e2a382a1e8ede5c08ed1c8044d9e
Salas4969 <salas.lorenzo4@gmail>
2021-05-29 12:07:48 -0400

    all works

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,64 @@
 var todayEl =document.getElementById("currentDay")
 var m = moment()
 var currentDay = m.format("dddd, MMMM Do YYYY")
-var currentHR = m.format("hA")
-var hours = [9,10,11,12,1,2,3,4,5]
-var hourIndex = 0
+var currentHR = m.format("h")
 var userInput = $(".input")
 console.log(currentHR)
-var timeslots =[$("time-9AM"),$("time-10AM"),$("time-11AM"),$("time-12PM"),$("time-1PM"),$("time-2PM"),$("time-3PM"),$("time-4PM"),$("time-5PM"),
+var timeslots =[$("#time-9AM"), $("#time-10AM"), $("#time-11AM"), $("#time-12PM"), $("#time-1PM"),$("#time-2PM"),$("#time-3PM"),$("#time-4PM"),$("#time-5PM"),
 ]
-var timeslotInd = 0
 todayEl.innerText=currentDay
 // display saved local storage 
-console.log(localStorage)
-userInput = localStorage.getItem("event")
+// console.log(localStorage)
+// var storedEvent = localStorage.getItem("event")
+// console.log(storedEvent)
+// userInput.val(storedEvent) 
 
 
 
 
 //compare var time slots to hours through index looping?
-for(var t=0 ; t < timeslots.length ; t++){
-
+for(var t=0 ; t < timeslots.length ; t++){ 
+    var slotHr = timeslots[t].attr("data-hour")
+    hourCheck(slotHr,timeslots[t])
+    
+}
 // give timeslot time correct time 
 // compare hours of each timeslot
 // change colo based on past,present,future
 // var slotHr =timeslots.hour();
-function hourCheck(){
-if(slotHr.isBefore(currentHR)){
-    $(timeslot).addClass("future")
+
+function hourCheck(slotHr,timeslot){
+    console.log(slotHr, timeslot)
+
+    if(parseInt(slotHr) > parseInt(currentHR)){
+        timeslot.addClass("future")
 }
-else if (slotHr === currentHR) {
-    $(timeslot).addClass("present")
+else if (parseInt(slotHr) === parseInt(currentHR)) {
+    timeslot.addClass("present")
 }
-else if (slotHr.isAfter(currentHR)){
-    $(timeslot).addClass("past")
-}}
+else if (parseInt(slotHr) < parseInt(currentHR)){
+    timeslot.addClass("past")
+}
+}
 
 
-}
 
 // save input to local storage
-$(".saveBtn").on('click', function(event){
-    event.stopPropagation();
-    event.stopImmediatePropagation();
-    var eventInputed = userInput.val()
-localStorage.setItem("event",eventInputed )
+$(function(){
+$(".saveBtn").on('click', function(){
+var timeslot = $(this).attr("data-hour")
+var inputArea =$("textarea[data-hour="+timeslot+"]")
+console.log(inputArea)
+var inputText = inputArea.val();
+var inputTime = inputArea.attr("data-hour");
+localStorage.setItem("time-"+ inputTime, inputText);
+
 console.log(localStorage)
+})
+for (var i=9; i< 17; i++){
+  var inputText = localStorage.getItem("time-"+ i)
+  var inputArea = $("textarea[data-hour="+i+"]")
+  inputArea.val(inputText)
+
+}
 })
